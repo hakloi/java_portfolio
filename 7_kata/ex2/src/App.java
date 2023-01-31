b3941a41b8cf55050864b8eff33ddcaf6e0eaa15
Violetta <murasakitta@gmail.com>
2023-02-01 00:53:23 +0300

    ex1 done

---
+++ 7_kata/ex2/src/App.java
@@ -0,0 +1,12 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // clean console
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+
+        // code begins
+
+        
+    }
+}
