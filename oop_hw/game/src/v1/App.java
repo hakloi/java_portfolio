18a5e57b1fd7d25a4e545349c65fa965a6070ae0
Violetta <murasakitta@gmail.com>
2023-04-11 15:57:47 +0300

    game oop ex

--- oop_hw/game/src/v1/App.java
+++ oop_hw/game/src/v1/App.java
@@ -1,6 +1,17 @@
 package v1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import v1.Healers.Wally;
+import v1.Warriors.Juno;
+import v1.Warriors.Monic;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        List<Person> team1 = new ArrayList<>();
+        team1.add(new Wally());
+        team1.add(new Juno());
+        team1.add(new Monic());
     }
 }
--- oop_hw/game/src/v1/Healers/Healer.java
+++ oop_hw/game/src/v1/Healers/Healer.java
@@ -1,5 +1,8 @@
 package v1.Healers;
+import v1.Person;
 
 public abstract class Healer extends Person {
-    
+    public void healing(){
+        
+    }
 }
