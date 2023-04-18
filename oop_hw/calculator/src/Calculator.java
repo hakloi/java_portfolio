a5b1f0fd64c0d7ac7be1bfbc511faef40f8a1a23
Violetta <murasakitta@gmail.com>
2023-04-18 16:01:47 +0300

    calculator + builder (?)

--- oop_hw/calculator/src/Calculator.java
+++ oop_hw/calculator/src/Calculator.java
@@ -1,18 +1,18 @@
 import java.util.Set;
 
 public class Calculator {
-    private Integer num1;
-    private Integer num2;
+    private double num1;
+    private double num2;
 
     private Calculator(){
         // private constructor
     }
 
-    public Integer getNum1() {
+    public double getNum1() {
         return num1;
     }
 
-    public Integer getNum2() {
+    public double getNum2() {
         return num2;
     }
 
@@ -26,12 +26,12 @@ public class Calculator {
             // private constructor - Метод возвращает уже готовый объект
         }
 
-        public Builder setNum1(Integer num1) {
+        public Builder setNum1(double num1) {
             Calculator.this.num1 = num1;
             return this;
         }
     
-        public Builder setNum2(Integer num2) {
+        public Builder setNum2(double num2) {
             Calculator.this.num2 = num2;
             return this;
         }
@@ -40,4 +40,5 @@ public class Calculator {
             return Calculator.this;
         }
         
+}
 }
