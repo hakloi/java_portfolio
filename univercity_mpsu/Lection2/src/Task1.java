d6625cbde94afd083820393edbf878e8851f6da4
Violetta <murasakitta@gmail.com>
2023-09-16 08:04:11 +0300

    task1 lec2

--- univercity_mpsu/Lection2/src/Task1.java
+++ univercity_mpsu/Lection2/src/Task1.java
@@ -1,5 +1,11 @@
 public class Task1 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //clean console
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        //code begins
+        double num = 23.2;
+        int result = (int)Math.round(num);
+        System.out.println(result);  
     }
 }
