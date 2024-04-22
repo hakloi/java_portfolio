2030bfdc4d7b54cb7d8857aeda894d315699d6d5
Violetta <murasakitta@gmail.com>
2024-04-22 13:36:01 +0300

    +service, ftlh

---
+++ spring_mpsu/hw6-7/src/main/java/com/example/buysell/models/Cipher.java
@@ -0,0 +1,23 @@
+package com.example.buysell.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Cipher {
+    private Long id;
+    private String title;
+    private String description;
+    private String creator;
+    private int yearCreated;
+    private EncryptionType type;
+    
+    public enum EncryptionType {
+        ASYNCHRONOUS,
+        SYNCHRONOUS,
+        HYBRID,
+        CLASSIC, 
+        WITHOUT_KEY
+    }
+}
---
+++ spring_mpsu/hw6-7/src/main/java/com/example/buysell/service/CipherService.java
@@ -0,0 +1,44 @@
+package com.example.buysell.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import com.example.buysell.models.Cipher;
+import com.example.buysell.models.Cipher.EncryptionType;
+
+@Component
+@Service
+public class CipherService {
+    private List<Cipher> ciphers = new ArrayList<>();  
+    private long id = 0;
+
+    {
+        ciphers.add(new Cipher(++id, "Jefferson disk", null, 
+        "Thomas Jefferson", 1790, EncryptionType.CLASSIC ));
+
+        ciphers.add(new Cipher(++id, "Caesar cipher", "a simple encryption technique that was used by Julius Caesar to send secret messages to his allies.", 
+        "Julius Caesar", 0, EncryptionType.CLASSIC ));
+    }
+
+    public List<Cipher> listCiphers() {
+        return ciphers;
+    }
+
+    public void saveCipher(Cipher cipher){
+        cipher.setId(++id);
+        ciphers.add(cipher);
+    }
+
+    public void deleteCipher(Long id){
+        ciphers.removeIf(cipher -> cipher.getId().equals(id));
+    }
+
+    public Cipher getCipherById(Long id) {
+        for (Cipher cipher : ciphers) {
+            if (cipher.getId().equals(id)) return cipher;
+        }
+        return null;
+    }
+}
